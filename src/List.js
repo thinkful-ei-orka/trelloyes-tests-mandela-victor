abeb26debec64afcb3db7c6577f521bc374c6acf
Victor Jarvis <vctrjrvs@gmail.com>
2020-05-11 16:49:08 -0400

    save

--- src/List.js
+++ src/List.js
@@ -4,22 +4,21 @@ import Card from './Card';
 import './List.css';
 
 
-export default function list (props)  {
+export default function list(props) {
     const cards = props.cards.map(card => (
         <Card key={card.id} title={card.title} content={card.content} />
     ));
-    
+
     return (
-            <section className="List">
-                <header className="List-header">
-                    <h2>{props.header}</h2></header>
-                <div className="List-cards">
+        <section className="List">
+            <header className="List-header">
+                <h2>{props.header}</h2></header>
+            <div className="List-cards">
                 {cards}
                 <button type="button" className="List-add-button">
-              + Add Random Card
+                    + Add Random Card
             </button>
-                </div>
-            </section>
-        );
+            </div>
+        </section>
+    );
 }
-
